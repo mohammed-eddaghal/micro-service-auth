eb3cb50c4408c2b9ab94b1bd92eafadf1251a9a6
mohammed-eddaghal <mohammed_eddaghal@um5.ac.ma>
2022-07-15 09:27:21 +0100

    add requests and responses

---
+++ src/main/java/com/mohammed/requests/RoleRequest.java
@@ -0,0 +1,14 @@
+package com.mohammed.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class RoleRequest {
+private String roleName;
+}
---
+++ src/main/java/com/mohammed/responses/RoleResponse.java
@@ -0,0 +1,17 @@
+package com.mohammed.responses;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@XmlRootElement
+public class RoleResponse {
+private String roleName;
+}
